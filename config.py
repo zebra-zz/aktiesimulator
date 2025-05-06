1b501192532ee55f3fbc2b8ce0bb4252bef0e2df
Claes Björklund <claes.bjorklund@gmail.com>
2025-05-06 23:12:45 +0200

    test

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'du-hemliga-nyckel'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///market.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
